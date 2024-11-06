90e202c5041e3dfe380f329bd114b7ffa4c98e2a
Alfvag <63364204+Alfvag@users.noreply.github.com>
2024-11-06 23:48:54 +0100

    Assignment 3 started

---
+++ SYSA21 Lab Assignment 3/src/Person.java
@@ -0,0 +1,47 @@
+public class Person {
+    private String identificationNumber;
+    private String name;
+    private int age;
+
+    public Person(String identificationNumber, String name, int age) {
+        this.identificationNumber = identificationNumber;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void addAccount(BankAccount account) {
+        // Add account to person
+    }
+
+    public BankAccount findAccount(String accountNumber) {
+        // Find account by account number
+    }
+
+    public double calculateTotalBalance() {
+        // Get balance of account
+    }
+}
